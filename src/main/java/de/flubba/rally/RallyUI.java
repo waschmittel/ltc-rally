5c93ad35632c0579af85fdfe3086430dddd7cc65
Daniel Flassak <daniel@flubba.de>
2018-04-12 22:25:28 +0200

    explicitly set session timeout for war deployment

--- src/main/java/de/flubba/rally/RallyUI.java
+++ src/main/java/de/flubba/rally/RallyUI.java
@@ -3,6 +3,7 @@ package de.flubba.rally;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.vaadin.teemusa.sidemenu.SideMenu;
 
 import com.vaadin.annotations.Push;
@@ -14,6 +15,7 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewDisplay;
 import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinSession;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.spring.annotation.SpringViewDisplay;
 import com.vaadin.spring.navigator.SpringNavigator;
@@ -21,8 +23,8 @@ import com.vaadin.spring.navigator.SpringViewProvider;
 import com.vaadin.ui.UI;
 
 import de.flubba.generated.i18n.I18n;
-import de.flubba.rally.view.ResultsView;
 import de.flubba.rally.view.LiveView;
+import de.flubba.rally.view.ResultsView;
 import de.flubba.rally.view.RunnerView;
 import de.flubba.rally.view.TagAssignmentsView;
 
@@ -32,6 +34,9 @@ import de.flubba.rally.view.TagAssignmentsView;
 @SpringUI
 @Viewport("width=device-width, initial-scale=1")
 public class RallyUI extends UI {
+    @Value("${server.session.timeout}")
+    private Integer sessionTimeout;
+
     @Autowired
     private SpringViewProvider springViewProvider;
 
@@ -40,11 +45,17 @@ public class RallyUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
+        setSessionTimeout();
         setLocale(Locale.UK); // because it's English but the first day of the week is Monday
         Page.getCurrent().setTitle(I18n.APPLICATION_TITLE.get());
 
         initNavigator();
         addSideMenu();
+    }
+
+    private void setSessionTimeout() {
+        // needs to be set if the application is deployed as a WAR instead of a jar
+        VaadinSession.getCurrent().getSession().setMaxInactiveInterval(sessionTimeout);
     }
 
     @Autowired
